061083eab1d4c70f222ac6446fec78343d6b98b1
Dennis Wenrich <dennisfinley@gmail.com>
2022-04-27 19:23:36 -0400

    intital commit

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,8 @@
+<h1>{{ $headings }}</h1>
+@if(count($listings) == 0)
+    <p>No Listings Found</p>
+@endif
+@foreach($listings as $listing)
+    <h2>{{ $listing['title']}}</h2>
+    <p>{{ $listing['description']}}</p>
+@endforeach
