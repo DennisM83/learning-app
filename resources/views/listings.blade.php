6bfb56a1e227cd74ef0edbf72ec5f7e254bed11e
Dennis Wenrich <dennisfinley@gmail.com>
2022-04-27 20:24:38 -0400

    add link to each listing

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -3,6 +3,6 @@
     <p>No Listings Found</p>
 @endif
 @foreach($listings as $listing)
-    <h2>{{ $listing['title']}}</h2>
+    <a href="/listings/{{$listing['id']}}"><h2>{{ $listing['title']}}</h2></a>
     <p>{{ $listing['description']}}</p>
 @endforeach
