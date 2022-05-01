b1c37e034773036646d6b8fa8894629db57e10c9
Dennis Wenrich <dennisfinley@gmail.com>
2022-05-01 12:08:42 -0400

    added store controller and redirect

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 // Show all listings
 class ListingController extends Controller
@@ -26,5 +28,22 @@ class ListingController extends Controller
     public function create()
     {
         return view('listings.create');
+    }
+
+    //Store Listing Data
+
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required',
+        ]);
+
+        return redirect('/');
     }
 }
