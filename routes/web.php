5296a5b9e6a07d6c4e1feb2a763b96fb1f386fe2
Dennis Wenrich <dennisfinley@gmail.com>
2022-05-02 16:58:13 -0400

    added route to edit and update form

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,12 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 // Store listing data
 Route::post('/listings', [ListingController::class, 'store']);
 
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Edit Submit To Update
+
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
