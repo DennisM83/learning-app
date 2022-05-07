471ee65bb951254ee6646090b1509d0f18ca3cb8
Dennis Wenrich <dennisfinley@gmail.com>
2022-05-07 09:54:57 -0400

    added a controller for login and auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,4 +42,28 @@ class UserController extends Controller
 
         return redirect('/')->with('message', 'You have logged out successfully');
     }
+
+    //Show login form
+    public function login()
+    {
+        return view('users.login');
+    }
+
+    //Authenticate user
+
+    public function authenticate(Request $request)
+    {
+        $formFields = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt($formFields)) {
+            $request->session()->regenerate();
+
+            return redirect('/')->with('message', 'You logged in successfully');
+        }
+
+        return back()->withErrors(['email' => 'Invalid Credentials'])->onlyInput('email');
+    }
 }
