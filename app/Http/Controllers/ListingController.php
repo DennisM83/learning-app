0ecf4afa374fd125c1841cf2b23e69aeae1c54ad
Dennis Wenrich <dennisfinley@gmail.com>
2022-05-08 09:24:28 -0400

    added user auth for deletion

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -60,8 +60,16 @@ class ListingController extends Controller
         return view('listings.edit', ['listing' => $listing]);
     }
 
+    // Update listing data
+
     public function update(Request $request, Listing $listing)
     {
+        //Make sure logged in user is owner
+
+        if ($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
         $formFields = $request->validate([
             'title' => 'required',
             'company' => 'required',
@@ -84,6 +92,10 @@ class ListingController extends Controller
     // Delete Listing
     public function destroy(Listing $listing)
     {
+        if ($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
         $listing->delete();
 
         return redirect('/')->with('message', 'Listing deleted successfully');
